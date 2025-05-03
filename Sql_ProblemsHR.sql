3c99681c13c4c46d4d9e70969a9c57d306971b71
Gopika Janarthanan <141640423+Gopika-Janarthanan@users.noreply.github.com>
2025-05-03 10:21:28 +0530

    Update and rename day_1_hacker rank to Sql_ProblemsHR.sql

--- Sql_ProblemsHR.sql
+++ Sql_ProblemsHR.sql
@@ -1,45 +1,45 @@
-# problem 1
+-- problem 1
 
-# Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
+--Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
 
-#solution:
+--solution:
 
 select*from city where population >100000 and countrycode='usa';
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-# problem 2
+--problem 2
 
-# Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
+--Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
 
-# solution:
+--solution:
 
 select name from city where population>120000  and countrycode='usa';
 
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
- # problem 3
+ --problem 3
 
- # Query all columns (attributes) for every row in the CITY table.
+ --Query all columns (attributes) for every row in the CITY table.
 
- # solution
+--solution
 
  select*from city;
 
  ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-# problem 4
+--problem 4
 
-# Query all columns for a city in CITY with the ID 1661.
+--Query all columns for a city in CITY with the ID 1661.
 
-# solution
+--solution
 
 select*from city where id=1661;
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-# problem 5
+--problem 5
 
-# Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
+--Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
 
-# solution
+--solution
 
 select*from city where countrycode='jpn';
 
